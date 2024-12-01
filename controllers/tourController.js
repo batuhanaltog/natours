4b3c897d9313bfd8ef87f203ed91cfb6d0dadf6b
batuhanaltog <batuhanaltog88@gmail.com>
2024-12-01 17:05:18 +0300

    first commit v2

---
+++ controllers/tourController.js
@@ -0,0 +1,50 @@
+const Tour = require("./../models/tourModel");
+
+exports.getAllTours = (req, res) => {
+  console.log(req.requestTime);
+  res.status(200).json({
+    status: 'success',
+    requestedAt: req.requestTime,
+    // results: tours.length,
+    // data: {
+    //   // tours:tours
+    //   tours,
+    // },
+  });
+};
+
+exports.getTour = (req, res) => {
+  const id = req.params.id * 1; //this is a trick that do convert a string into number
+  // const tour = tours.find((el) => el.id === id);
+  // res.status(200).json({
+  //   status: 'success',
+  //   data: {
+  //     tour,
+  //   },
+  // });
+};
+
+exports.createTour = (req, res) => {
+  res.status(201).json({
+    status: 'success',
+    // data: {
+    //   tour: newTour,
+    // },
+  });
+};
+
+exports.updateTour = (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: '<Updated tour>',
+    },
+  });
+};
+
+exports.deleteTour = (req, res) => {
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+};
